925fa817b6c3278c8da37e683435d1f05fb5512f
Nguyen Huy Hung <hungnh103@gmail.com>
2018-08-17 11:23:52 +0700

    user login logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def logged_in_user
+    return if logged_in?
+    flash[:danger] = t "login"
+    redirect_to login_url
+  end
 end
