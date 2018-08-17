c244e931e817869d219d7501e2842686109d7117
Nguyen Huy Hung <hungnh103@gmail.com>
2018-08-17 11:23:52 +0700

    users signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   resources :products, only: %i(show)
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
 end
